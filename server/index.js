36a4a852892baf7dc5412453e7a59320eafe9771
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-12-19 15:19:37 +0600

    server commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-node_modules
+import express from 'express';
+const app = express();
+const PORT = 5000;
+app.get('/', (req, res) => {
+    res.send('Threadly is running!');
+    });
+    app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    });
